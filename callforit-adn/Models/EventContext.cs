9a09f6613b24049cb5fa709c36dc67f16c456970
Chris Weldon <chris@chrisweldon.net>
2013-03-01 08:07:03 -0600

    Updating connection strings.

--- callforit-adn/Models/EventContext.cs
+++ callforit-adn/Models/EventContext.cs
@@ -4,6 +4,9 @@ namespace callforit_adn.Models
 {
     public class EventContext : DbContext
     {
+        public EventContext() : base("callforit")
+        {
+        }
         public DbSet<Event> Events { get; set; }
     }
 }
