2a77ada520f38ca19291d5b97e4f5acab4fcd3b4
Chris Weldon <chris@chrisweldon.net>
2013-03-01 07:07:10 -0600

    Enabling migrations.

---
+++ callforit-adn/Migrations/201303011305518_Initial.cs
@@ -0,0 +1,27 @@
+namespace callforit_adn.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Events",
+                c => new
+                    {
+                        EventId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        DateStart = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.EventId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Events");
+        }
+    }
+}
---
+++ callforit-adn/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace callforit_adn.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<callforit_adn.Models.EventContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(callforit_adn.Models.EventContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
